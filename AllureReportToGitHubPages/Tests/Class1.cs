7706698fdfe204cfd70e7dd9074045f0c96e6980
Sergey Vinogradov <vins@wildapricot.com>
2021-01-10 13:31:40 +0300

    no message

---
+++ AllureReportToGitHubPages/Tests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using NUnit.Allure.Core;
+
+namespace Tests
+{
+    using NUnit.Framework;
+    [TestFixture]
+    [AllureNUnit]
+    public class TestsExamples
+    {
+        [Test]
+        [Category("smoke")]
+        public void GetCompanyInvoices()
+        {
+            Thread.Sleep(1000);
+            Assert.Fail("test fail");
+        }
+
+        [Test]
+        [Category("smoke")]
+        public void GetCompanyInvoices2()
+        {
+            Thread.Sleep(1000);
+            Assert.Pass("test pass");
+        }
+    }
+}
